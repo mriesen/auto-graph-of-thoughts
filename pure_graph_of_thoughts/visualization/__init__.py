f7e8041a11f585c9b6e8975cbdf3d0576040461c
Manuel Riesen <manuelnoah.riesen@students.bfh.ch>
2024-03-02 17:43:36 +0100

    feat(got-viz): implement basic graph visualization

---
+++ pure_graph_of_thoughts/visualization/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from .graph_visualizer import plot_graph
+except ImportError:
+    raise ImportError(
+            'The visualisation module requires the optional visualization dependencies. '
+            'Install with: pip install auto-graph-of-thoughts[visualization]'
+            '\n'
+            'To get a hierarchical visualization, install: '
+            'pip install auto-graph-of-thoughts[visualization,dot-visualization]'
+    )
