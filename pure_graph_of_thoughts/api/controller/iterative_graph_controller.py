01ab6007192d3f4edc9cb182a199e4dee87450ed
Manuel Riesen <manuelnoah.riesen@students.bfh.ch>
2024-03-08 11:52:04 +0100

    feat(got-api): implement simple iterative graph controller

---
+++ pure_graph_of_thoughts/api/controller/iterative_graph_controller.py
@@ -0,0 +1,18 @@
+from ..controller import Controller
+from ..controller.graph_of_operations_execution import GraphOfOperationsExecution
+from ..graph.operation import OperationNode
+
+
+class IterativeGraphController(Controller):
+    """
+    A controller for executing a graph of operations iteratively.
+    The controller executes operations on an operation node basis.
+    """
+
+    def execute(self, operation_node: OperationNode, execution: GraphOfOperationsExecution) -> None:
+        """
+        Executes a given operation node in the context of an ongoing execution.
+        :param operation_node: operation node to execute
+        :param execution: ongoing execution
+        """
+        execution.process_operation(operation_node, self._process_operation_node)
