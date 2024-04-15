37736c9943f095e5b658af01acf4bbe5f8ff1b78
Manuel Riesen <manuelnoah.riesen@students.bfh.ch>
2024-04-15 15:39:23 +0200

    feat(got-viz): use proper labels for node visualization

--- pure_graph_of_thoughts/visualization/plot_graph.py
+++ pure_graph_of_thoughts/visualization/plot_graph.py
@@ -1,6 +1,6 @@
 import importlib.util
 from dataclasses import dataclass, field
-from typing import TypeVar, Generic, Any, Callable, cast, Union, Optional
+from typing import TypeVar, Generic, Any, Callable, cast, Union, Optional, Dict, Tuple
 
 import networkx as nx
 from matplotlib import pyplot as plt
@@ -9,6 +9,7 @@ from networkx.drawing.nx_agraph import graphviz_layout
 from ..api.graph import Graph, Node, GraphSchema
 from ..api.graph.operation import OperationNode, GraphOfOperations
 from ..api.graph.thought import ThoughtNode, GraphOfThoughts
+from ..api.internal.id import Id
 
 _N = TypeVar('_N', bound=Node)
 """The type of node"""
@@ -26,38 +27,43 @@ class _GraphVisualizer(Generic[_T, _N]):
     Visualizer for graphs.
     """
 
-    _transform_node: Callable[[_N], _T] = field(default=lambda node: cast(_T, node))
-    """Transform function to apply to a node before constructing a networkx graph"""
+    _node_label: Callable[[_N], _T] = field(default=lambda node: cast(_T, node))
+    """Function to apply to a node to get the label of the node"""
 
     @staticmethod
     def _create_networkx_graph(
             graph: Graph[_N, _S],
-            transform_node: Callable[[_N], _T] = lambda node: cast(_T, node)
-    ) -> nx.DiGraph:  # type: ignore
+            node_label: Callable[[_N], _T] = lambda node: cast(_T, node)
+    ) -> Tuple[Dict[Id, _T], nx.DiGraph]:  # type: ignore
         nx_graph = nx.DiGraph()  # type: ignore
-        nodes = list(map(transform_node, graph.nodes))
-        edges = [(transform_node(edge[0]), transform_node(edge[1])) for edge in graph.edges]
+        nodes = graph.nodes
+        edges = graph.edges
+        node_ids = [node.id for node in nodes]
+        edge_ids = [(edge[0].id, edge[1].id) for edge in edges]
+        labels = {
+            node.id: node_label(node) for node in nodes
+        }
 
-        nx_graph.add_nodes_from(nodes)
-        nx_graph.add_edges_from(edges)
-        return nx_graph
+        nx_graph.add_nodes_from(node_ids)
+        nx_graph.add_edges_from(edge_ids)
+        return labels, nx_graph
 
-    def plot_graph(self, graph: Graph[_N, _S], custom_transform_node: Optional[Callable[[_N], _T]] = None) -> None:
+    def plot_graph(self, graph: Graph[_N, _S], custom_node_label: Optional[Callable[[_N], _T]] = None) -> None:
         """
         Plots a given graph.
         :param graph: graph to plot
-        :param custom_transform_node: custom transform node function to apply
+        :param custom_node_label: custom node label function to apply
         """
 
-        transform_node = self._transform_node if custom_transform_node is None else custom_transform_node
+        node_label = self._node_label if custom_node_label is None else custom_node_label
 
         plt.tight_layout()
-        nx_graph = _GraphVisualizer._create_networkx_graph(graph, transform_node)
+        labels, nx_graph = _GraphVisualizer._create_networkx_graph(graph, node_label)
         if _GraphVisualizer._is_pygraphviz_available():
             pos = graphviz_layout(nx_graph, prog='dot')
-            nx.draw_networkx(nx_graph, pos=pos, arrows=True)
+            nx.draw_networkx(nx_graph, pos=pos, arrows=True, labels=labels)
         else:
-            nx.draw_networkx(nx_graph, arrows=True)
+            nx.draw_networkx(nx_graph, arrows=True, labels=labels)
         plt.show()
 
     @staticmethod
@@ -66,11 +72,11 @@ class _GraphVisualizer(Generic[_T, _N]):
 
 
 _graph_of_operations_visualizer = _GraphVisualizer[str, OperationNode](
-        lambda node: f'{node.id}-{node.operation.name}'
+        lambda node: f'{node.operation.name}'
 )
 
 _graph_of_thoughts_visualizer = _GraphVisualizer[str, ThoughtNode](
-        lambda node: f'{node.id}-{node.thought.state}'
+        lambda node: f'{node.thought.state}'
 )
 
 
