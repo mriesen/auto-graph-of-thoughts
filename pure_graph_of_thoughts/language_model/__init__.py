aacf1bb3d2660430e87a272954d9963f4ce00a3f
Manuel Riesen <manuelnoah.riesen@students.bfh.ch>
2024-04-16 15:54:57 +0200

    feat(got-lm): implement simulated language model

--- pure_graph_of_thoughts/language_model/__init__.py
+++ pure_graph_of_thoughts/language_model/__init__.py
@@ -1,2 +1,3 @@
 from .chatgpt import ChatGPT, GPTModel, GPTCost
 from .mock_language_model import MockLanguageModel
+from .simulated_language_model import SimulatedLanguageModelBehavior, SimulatedLanguageModel
